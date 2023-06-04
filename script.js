d8c5c6a9a817c48f47fcad35bdeb2121cade48f1
dlaehrbs <102680859+dlaehrbs@users.noreply.github.com>
2023-06-04 20:32:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+
+
+
+
+
 ///로그인 정보 저장 변수
 const loginForm = document.querySelector("#login");
 
 const loginInput1 = document.querySelector("#name");
 const loginInput2 = document.querySelector("#student_number");
 const loginInput3 = document.querySelector("#seat_number");
+
+
 
 ////텍스트 출력 변수
 const greeting1 = document.querySelector("#greeting1");
@@ -33,8 +40,10 @@ if (savedUsername === null){ ////저장된 정보가 없으면
   paintGreetings1(savedUsername)
   paintGreetings2(savedStudent_number) ///아니면 저장된 이름으로 pain~~함수 실행
   paintGreetings3(savedSeat_number)
-  removeBtn.classList.remove("hidden")
-  information.classList.remove("hidden")
+
+  loginForm.classList.add("hidden");
+  removeBtn.classList.remove("hidden")  /// 정보제거버튼의 hidden 클래스를 제거
+  information.classList.remove("hidden")  /// 로그인폼의 hidden 클래스를 제거
   greeting4.innerText = "반드시 캡쳐해주세요"
 }
 
@@ -50,6 +59,11 @@ function onLoginSubmit(event) {
   const student_number = loginInput2.value;  ///학번
   const seat_number = loginInput3.value; ///자리번호
 
+  const SEAT_NUMBER = seat_number.toUpperCase(); /// 자리번호 입력값을 대문자로 바꾸기
+  seatInformtion(SEAT_NUMBER)   ///대문자로 바뀐 값으로 seatInformtion함수 실행
+
+
+
   localStorage.setItem("username", username);  ///이름저장
   localStorage.setItem("student_number",student_number) ///학번저장
   localStorage.setItem("seat_number",seat_number) ///자리번호 저장
@@ -61,8 +75,19 @@ function onLoginSubmit(event) {
   removeBtn.classList.remove("hidden") ///뒤로가기 버튼 보이게 하기
   information.classList.remove("hidden") ///학번 이름 자리번호 보이게하기
   greeting4.innerText = "반드시 캡쳐해주세요"
+
+  
+
 }
 
+
+/// 자리 색깔 바꾸기 함수
+function seatInformtion(SEAT_NUMBER){
+
+    const seatInformtion = document.getElementById(SEAT_NUMBER)
+    seatInformtion.classList.remove("seat")
+    seatInformtion.classList.add("selectedSeat")
+}
 
 
 
@@ -100,4 +125,7 @@ window.location.reload();
 }
 
 removeBtn.addEventListener("click", removeStorage);
+
+
+
 
